2709f580d7e9c2cb3a81d9b69163caf7402e70e0
TheMasterOfBox <30931935+MysteryBoxOfHell@users.noreply.github.com>
2020-04-14 22:39:39 -0500

    Cambios totales

---
+++ JavaScript/ServiJava.js
@@ -0,0 +1,35 @@
+function mostrarBMW(){
+  document.getElementById('AutoBMW').style.display="block";
+  document.getElementById('AutoChevrolet').style.display="none";
+  document.getElementById('AutoHonda').style.display="none";
+  document.getElementById('AutoMazda').style.display="none";
+  document.getElementById('AutoRenault').style.display="none";
+}
+function mostrarChevrolet(){
+  document.getElementById('AutoBMW').style.display="none";
+  document.getElementById('AutoChevrolet').style.display="block";
+  document.getElementById('AutoHonda').style.display="none";
+  document.getElementById('AutoMazda').style.display="none";
+  document.getElementById('AutoRenault').style.display="none";
+}
+function mostrarHonda(){
+  document.getElementById('AutoBMW').style.display="none";
+  document.getElementById('AutoChevrolet').style.display="none";
+  document.getElementById('AutoHonda').style.display="block";
+  document.getElementById('AutoMazda').style.display="none";
+  document.getElementById('AutoRenault').style.display="none";
+}
+function mostrarMazda(){
+  document.getElementById('AutoBMW').style.display="none";
+  document.getElementById('AutoChevrolet').style.display="none";
+  document.getElementById('AutoHonda').style.display="none";
+  document.getElementById('AutoMazda').style.display="block";
+  document.getElementById('AutoRenault').style.display="none";
+}
+function mostrarRenault(){
+  document.getElementById('AutoBMW').style.display="none";
+  document.getElementById('AutoChevrolet').style.display="none";
+  document.getElementById('AutoHonda').style.display="none";
+  document.getElementById('AutoMazda').style.display="none";
+  document.getElementById('AutoRenault').style.display="block";
+}
